414977867f8178789c0e96961d3a52f8b9462018
Daniel Machado <daniel.machado.av@gmail.com>
2021-03-04 13:17:51 -0300

    Chess layer and printing the board

---
+++ src/application/UI.java
@@ -0,0 +1,29 @@
+package application;
+
+import chess.ChessPiece;
+
+public class UI {		
+	public static void printBoard(ChessPiece[][] pieces) {
+		
+		System.out.println("\n");
+		
+		for(int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " "); // << Appends of first line || Always BEFORE the little for entry
+			for(int j = 0; j < pieces.length; j++) { // << pieces.lenght aswell because this is a quadratic matrix
+				printPiece(pieces[i][j]);								
+			}
+			System.out.println();
+		}
+		System.out.println("  A B C D E F G H");
+	}
+	
+	private static void printPiece(ChessPiece piece) {
+		if(piece == null) {
+			System.out.print("-");
+		}
+		else {
+			System.out.print(piece);
+		}
+		System.out.print(" "); // << after each piece printed there will be a blanc space to make the pieces separation
+	}
+}
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -24,4 +24,11 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position posistion) {
+		return pieces[posistion.getRow()][posistion.getColumn()];
+	}
 }
