68f3515d39f493b55b4ed3908642611befe50fbe
Daniel Machado <daniel.machado.av@gmail.com>
2021-03-05 15:55:09 -0300

    BoardException and defensive programming

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,13 +8,13 @@ public class UI {
 		System.out.println("\n");
 		
 		for(int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " "); // << Appends of first line || Always BEFORE the little for entry
+			System.out.print("   " + (8 - i) + " "); // << Appends of first line || Always BEFORE the little for entry
 			for(int j = 0; j < pieces.length; j++) { // << pieces.lenght aswell because this is a quadratic matrix
 				printPiece(pieces[i][j]);								
 			}
 			System.out.println();
 		}
-		System.out.println("  A B C D E F G H");
+		System.out.println("     A B C D E F G H");
 	}
 	
 	private static void printPiece(ChessPiece piece) {
